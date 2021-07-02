77cbfcb6db6cd0700d0b23a151f916702530740c
fecet <xiezej@gmail.com>
2021-07-02 14:30:42 +0800

    ranger

--- ranger/commands.py
+++ ranger/commands.py
@@ -120,12 +120,14 @@ class fzf_select(Command):
         import subprocess
         if self.quantifier:
             # match only directories
-            command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
-            -o -type d -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
+            #command="fd -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            #-o -type d -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
+            command='fd --hidden --follow -E ".git" -E "/run/timeshift" -E "/mnt/win/Windows" | fzf +m'
         else:
             # match files and directories
-            command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
-            -o -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
+            #command="fd -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            #-o -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
+            command='fd --hidden --follow -E ".git" -E "/run/timeshift" -E "/mnt/win/Windows" | fzf +m'
         fzf = self.fm.execute_command(command, stdout=subprocess.PIPE)
         stdout, stderr = fzf.communicate()
         if fzf.returncode == 0:
