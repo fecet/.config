9a81ef50ab5fb41fa84d4ed8ac4e4757312da4bb
fecet <xiezej@gmail.com>
2022-01-07 02:25:52 +0800

    upd

---
+++ clash/start-clash.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# save this file to ${HOME}/.config/clash/start-clash.sh
+
+# save pid file
+echo $$ > ${HOME}/.config/clash/clash.pid
+
+diff ${HOME}/.config/clash/config.yaml <(curl -s ${CLASH_URL})
+if [ "$?" == 0 ]
+then
+    /usr/bin/clash
+else
+    TIME=`date '+%Y-%m-%d %H:%M:%S'`
+    cp ${HOME}/.config/clash/config.yaml "${HOME}/.config/clash/config.yaml.bak${TIME}"
+    curl -L -o ${HOME}/.config/clash/config.yaml ${CLASH_URL}
+    /usr/bin/clash
+fi
