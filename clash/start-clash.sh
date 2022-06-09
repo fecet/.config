4db4c52dc097622163a66c0e00c8a3e83551b623
fecet <xiezej@gmail.com>
2022-06-09 16:48:48 +0800

    upd

--- clash/start-clash.sh
+++ clash/start-clash.sh
@@ -3,15 +3,15 @@
 
 # save pid file
 echo $$ > ${HOME}/.config/clash/clash.pid
-CLASH_URL="https://pub-api-1.bianyuan.xyz/sub?target=clash&url=https%3a%2f%2fsubscribe.a9b.top%2flink%2fupunzh6guh7szov6%3fclash%3d1&insert=false"
+CLASH_URL="https://subscribe.y9d3.top/link/UpuNZH6guh7sZov6?clash=1"
 
 diff ${HOME}/.config/clash/config.yaml <(curl -s ${CLASH_URL})
-if [ "$?" == 0 ]
+if [ "$0" == 0 ]
 then
     /usr/bin/clash
 else
-    # TIME=`date '+%Y-%m-%d %H:%M:%S'`
-    # cp ${HOME}/.config/clash/config.yaml "${HOME}/.config/clash/config.yaml.bak${TIME}"
-    # curl -L -o ${HOME}/.config/clash/config.yaml ${CLASH_URL}
+    TIME=`date '+%Y-%m-%d %H:%M:%S'`
+    cp ${HOME}/.config/clash/config.yaml "${HOME}/.config/clash/config.yaml.bak${TIME}"
+    curl -L -o ${HOME}/.config/clash/config.yaml ${CLASH_URL}
     /usr/bin/clash
 fi
