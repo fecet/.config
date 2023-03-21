3413b668837fe0d5bc2621d03aa370b844ed2add
fecet <xiezej@gmail.com>
2023-03-21 21:49:25 +0800

    upd

--- clash/stop-clash.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# save this file to ${HOME}/.config/clash/stop-clash.sh
-
-# read pid file
-PID=`cat ${HOME}/.config/clash/clash.pid`
-kill -9 ${PID}
-rm ${HOME}/.config/clash/clash.pid
